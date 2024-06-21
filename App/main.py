0ddc2ab7378bc54ec498cb2db5a115a4961f8fb0
Carlos Lopera B <carloslopera25@gmail.com>
2024-06-21 10:49:46 -0400

    App con libreria Pandas

--- App/main.py
+++ App/main.py
@@ -1,26 +1,39 @@
 import utils
 import read_csv
 import charts
+import pandas
+
+# En la clase de Pandas, reescribimos el código para ser utilizado con la librería Pandas
 
 def run():
-  data = read_csv.read_csv('data.csv')
+  '''
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
   countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
-  charts.generate_pie_chart(countries, percentages)
   
-  country = input('Type Country => ')
-  print(country)
+  '''
+  
+  #Realizamos el ajuste de código con Pandas
+  # En Pandas se utliza los "dataFrames" (df) 
+  df = pandas.read_csv('data.csv')
+  # Para filtrar:
+  df = df[df['Continent'] == 'South America']
+  #Para obtener la lista de paises y sus porcentajes:
+  paises = df['Country'].values
+  porcentajes = df['World Population Percentage'].values
 
+  charts.generate_pie_chart(countries, percentages)
+
+  data = read_csv.read_csv('data.csv')
+  country = input('Type Country => ')
 
   result = utils.population_by_country(data, country)
 
   if len(result) > 0:
     country = result[0]
-    print(country)
     labels, values = utils.get_population(country)
-    charts.generate_bar_chart(country['Country'], labels, values)
+    charts.generate_bar_chart(labels, values)
   
 
 if __name__ == '__main__':
