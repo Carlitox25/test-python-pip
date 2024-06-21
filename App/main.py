44c9324595e53551bce85cb2b8a00a5b24f5e2f9
Carlos Lopera B <carloslopera25@gmail.com>
2024-06-21 15:38:55 -0400

    corregido

--- App/main.py
+++ App/main.py
@@ -20,21 +20,23 @@ def run():
   # Para filtrar:
   df = df[df['Continent'] == 'South America']
   #Para obtener la lista de paises y sus porcentajes:
-  paises = df['Country'].values
-  porcentajes = df['World Population Percentage'].values
-
-  charts.generate_pie_chart(paises, porcentajes)
+  countries = df['Country'].values
+  percentages = df['World Population Percentage'].values
+  print(countries)
+  print(percentages)
+  #charts.generate_pie_chart(countries, percentages)
 
   data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
 
   result = utils.population_by_country(data, country)
-
+  print(result)
   if len(result) > 0:
     country = result[0]
+    print(country)
     labels, values = utils.get_population(country)
     print(labels,values)
-    charts.generate_bar_chart(labels, values)
+    charts.generate_bar_chart(country['Country'], labels, values)
   
 
 if __name__ == '__main__':
