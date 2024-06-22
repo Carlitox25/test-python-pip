5497988eb4a7f54688490be214ccd1a6eff558fb
Carlos Lopera B <carloslopera25@gmail.com>
2024-06-21 22:39:51 -0400

    App to app

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,17 @@
 import store
+from fastapi import FastAPI
+
+app1 = FastAPI()
+
+@app1.get('/')
+def get_list():
+    return[1,2,3]
+
+@app1.get('/contact')
+def get_list():
+    return{'name' : 'Platzi'}
+
+
 
 def run():
     store.getCategories()
